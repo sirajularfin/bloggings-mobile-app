9121152a915ec6b655da46a10cff98502c813e38
Sirajul Arfin <sirajularfin@gmail.com>
2024-11-01 00:18:00 +0530

    Hide splash screen when App component mounts

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import AppNavigator from './navigation/appNavigator';
+import AppNavigator from 'src/navigation/appNavigator';
 import { StyleSheet } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    // Hide the splash screen after the app component is mounted
+    SplashScreen.hide();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <AppNavigator />
